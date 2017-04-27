58c5a6cf1369966bb531504313ccb6d66a927e07
stephen paul <stephenpaul2727@gmail.com>
2017-04-27 15:11:43 -0400

    minor changes to upload file

--- upload.php
+++ upload.php
@@ -147,7 +147,7 @@ if (isset($_POST['x'])){
 
 		//Insert image into database
 		$insert_pic_query = mysqli_query($con, "UPDATE users SET profile_pic='$result_path' WHERE username='$userLoggedIn'");
-		header("Location: ".$userLoggedIn);
+		header("Location:profile.php?profile_username=".$userLoggedIn);
 														
 }// post x
 ?>
