e9d7654f59bc145301e0a91e80b4b2e3ce4432e7
stephen paul <stephenpaul2727@gmail.com>
2017-04-27 15:05:25 -0400

    Minor Changes to Post.php

--- post.php
+++ post.php
@@ -10,10 +10,10 @@ else {
 ?>
 
 <div class="user_details column">
-		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
+		<a href="<?php echo "profile.php?profile_username=".$userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
 		<div class="user_details_left_right">
-			<a href="<?php echo $userLoggedIn; ?>">
+			<a href="<?php echo "profile.php?profile_username=".$userLoggedIn; ?>">
 			<?php 
 			echo $user['first_name'] . " " . $user['last_name'];
 
